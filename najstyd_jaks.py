6a0faec9a29993e68a55c3e8d5a44db3ffd39546
berteserop <79014596+berteserop@users.noreply.github.com>
2021-02-26 21:45:44 +0300

    Add files via upload

---
+++ najstyd_jaks.py
@@ -0,0 +1,8 @@
+def model():
+	n = input("число:")
+	h = input("число 2:")
+	print(int(n) + int(h))
+def nas():
+        ns = input("ВЫ гей:")
+        nb = input(ns + " точно:")
+        bs = print("фу вы г*й!")
